946078b091b039fa42da303d0e524291e3f0ff9c
Ryan Coe <rcoe@sandia.gov>
2020-09-16 10:04:42 -0600

    do three sea states

--- codesign/workingversion.m
+++ codesign/workingversion.m
@@ -7,8 +7,7 @@
 clear
 close all
 
-optimOpts = optimoptions('fminunc',...
-    'MaxFunctionEvaluations',1e6, 'MaxIterations', 1e6, 'Display', 'off');
+
 
 %% Load WEC device data
 
@@ -24,109 +23,144 @@ Zpto = PTO_Impedance(w,[1, 0, 0, 0, sqrt(2/3), 1e-3, 0]); % [N, Id, Bd, Kd, Kt, 
 
 %% Define sea state and excitation
 
+% Hs = 0.125;
+% Tp = 2;
+% gamma = 3.3;
+%
+% S = jonswap(w, [Hs, Tp, gamma]);    % Wave energy density spectrum
+% A = sqrt(2*dw*S.S(:));              % wave amplitude spectrum
+% Fe = A .* Hex(:);
+
+
 Hs = 0.125;
-Tp = 2;
 gamma = 3.3;
+Tp = [1.58, 2.5, 3.5];
+for ii = 1:length(Tp)
+    S = jonswap(w, [Hs, Tp(ii), gamma]);    % Wave energy density spectrum
+    A = sqrt(2*dw*S.S(:));              % wave amplitude spectrum
+    Fe(:,ii) = A .* Hex(:);
+    Fe_name{ii} = sprintf('Tp%.2f',Tp(ii));
+end
 
-S = jonswap(w, [Hs, Tp, gamma]);    % Wave energy density spectrum
-A = sqrt(2*dw*S.S(:));              % wave amplitude spectrum
-Fe = A .* Hex(:);
+% Fe(:,4) = sqrt(8*real(Zi))*1; % const. power excitation
+% Fe_name{4} = 'constPowerExc';
+% 
+% Fe(:,5) = ones(size(Fe(:,1)))*mean(Fe(:,1));
+% Fe_name{5} = 'constFe';
 
-Pmax = abs(Fe).^2 ./ (8*real(Zi));
+
 
 %% Design controllers
 
-%---------------------------------
-wc(1).leg = 'CC on mech';
-wc(1).ZL = Zi2ZL(Zpto, conj(Zi));
-
-%---------------------------------
-wc(2).leg = 'PI on mech';
-wc(2).cinfo.type = 'PI';
-wc(2).cinfo.w = w;
-wc(2).cinfo.x0 = ones(1,2)*0.1;
-wc(2).objfun = @(x) Pmech( Zi2ZL(Zpto,fbc(x,wc(2).cinfo)),...
-    Zpto,...
-    Zi,Fe );
-[wc(2).y, wc(2).fval] = fminunc(wc(2).objfun, wc(2).cinfo.x0, optimOpts);
-wc(2).ZL = Zi2ZL(Zpto,fbc(wc(2).y, wc(2).cinfo));
-
-%---------------------------------
-wc(3).leg = 'CC on elec';
-wc(3).ZL = conj( squeeze(Zpto(2,2,:)) ...
-    - squeeze(Zpto(1,2,:)) .* squeeze(Zpto(2,1,:)) ...
-    ./ (squeeze(Zpto(1,1,:)) + Zi) );
-
-%---------------------------------
-wc(4).leg = 'PI on elec';
-wc(4).cinfo.type = 'PI';
-
-wc(4).cinfo.w = w;
-wc(4).cinfo.x0 = ones(1,2);
-wc(4).objfun = @(x) Pelec( Zi2ZL(Zpto,fbc(x,wc(4).cinfo)),...
-    Zpto,...
-    Zi,Fe );
-[wc(4).y, wc(4).fval] = fminunc(wc(4).objfun, wc(4).cinfo.x0, optimOpts);
-wc(4).ZL = Zi2ZL(Zpto,fbc(wc(4).y, wc(4).cinfo));
-
-
-%% Calculate power and efficiency
-
-for ii = 1:length(wc)
-    
-    % evaluate mech performance
-    [Pmech_tot(ii), mPmech(:,ii)] = Pmech(wc(ii).ZL, Zpto, Zi, Fe);
-    assert(-1*Pmech_tot(ii) <= sum(Pmax),...
-        sprintf('''%s'' making more mechanical power than theoretical limit',wc(1).leg))
-    
-    % evaluate mech performance
-    [Pelec_tot(ii), mPelec(:,ii)] = Pelec(wc(ii).ZL, Zpto, Zi, Fe);
-    assert(-1*Pelec_tot(ii) <= sum(Pmax),...
-        sprintf('''%s'' making more mechanical power than theoretical limit',wc(1).leg))
-    
-    legCel{ii} = wc(ii).leg;
+for ii = 1:size(Fe,2)
+    T{ii} = myfunc(Fe(:,ii),Zi,Zpto,w);
+    vn = T{ii}.Properties.VariableNames;
+    for jj = 1:length(vn)
+        vn{jj} = sprintf('%s_%.2f',vn{jj},Tp(ii));
+    end
+    T{ii}.Properties.VariableNames = vn;
 end
 
-eta_mech = Pmech_tot./(-1 * sum(Pmax));
-eta_elec = Pelec_tot./(-1 * sum(Pmax));
-
-%% Tabulate results
-
-T = table(-1*Pmech_tot(:)/1e3,eta_mech(:),-1*Pelec_tot(:)/1e3,eta_elec(:),...
-    'VariableNames',{'MechPow_kW','MechEfficiency','ElecPow_kW','ElecEffciency'},...
-    'RowNames',legCel);
-disp(T)
-
-clear input
-input.data = T{:,1:end};
-input.dataFormat = {'%.2g',};
-input.tableColumnAlignment = 'c';
-input.tableBorders = 0;
-input.makeCompleteLatexDocument = 0;
-input.tableRowLabels = T.Properties.RowNames;
-input.tableCaption = 'Text caption';
-input.tableLabel = 'tab:TestLabel';
-input.tableColLabels = T.Properties.VariableNames;
-latex = latexTable(input);
-
-%% Plot results
-
-figure('name','Frequency dependent power')
-hold on
-grid on
-plot(mPmech,'-')
-ax = gca;
-ax.ColorOrderIndex = 1;
-plot(mPelec,'--')
-
-figure('name','Bode plot')
-hold on
-grid on
-opt = bodeoptions;
-opt.FreqUnits = 'Hz';
-opt.Grid = 'on';
-for ii = 1:length(wc)
-    bodeplot(frd(wc(ii).ZL,w),opt)
-    legCel{ii} = wc(ii).leg;
+function T = myfunc(Fe,Zi,Zpto,w)
+    
+    optimOpts = optimoptions('fminunc',...
+        'MaxFunctionEvaluations',1e6, 'MaxIterations', 1e6, 'Display', 'off');
+    
+    Pmax = abs(Fe).^2 ./ (8*real(Zi));
+    
+    %---------------------------------
+    wc(1).leg = 'CC on mech';
+    wc(1).ZL = Zi2ZL(Zpto, conj(Zi));
+    
+    %---------------------------------
+    wc(2).leg = 'PI on mech';
+    wc(2).cinfo.type = 'PI';
+    wc(2).cinfo.w = w;
+    wc(2).cinfo.x0 = ones(1,2)*0.1;
+    wc(2).objfun = @(x) Pmech( Zi2ZL(Zpto,fbc(x,wc(2).cinfo)),...
+        Zpto,...
+        Zi,Fe );
+    [wc(2).y, wc(2).fval] = fminunc(wc(2).objfun, wc(2).cinfo.x0, optimOpts);
+    wc(2).ZL = Zi2ZL(Zpto,fbc(wc(2).y, wc(2).cinfo));
+    
+    %---------------------------------
+    wc(3).leg = 'CC on elec';
+    wc(3).ZL = conj( squeeze(Zpto(2,2,:)) ...
+        - squeeze(Zpto(1,2,:)) .* squeeze(Zpto(2,1,:)) ...
+        ./ (squeeze(Zpto(1,1,:)) + Zi) );
+    
+    %---------------------------------
+    wc(4).leg = 'PI on elec';
+    wc(4).cinfo.type = 'PI';
+    
+    wc(4).cinfo.w = w;
+    wc(4).cinfo.x0 = ones(1,2);
+    wc(4).objfun = @(x) Pelec( Zi2ZL(Zpto,fbc(x,wc(4).cinfo)),...
+        Zpto,...
+        Zi,Fe );
+    [wc(4).y, wc(4).fval] = fminunc(wc(4).objfun, wc(4).cinfo.x0, optimOpts);
+    wc(4).ZL = Zi2ZL(Zpto,fbc(wc(4).y, wc(4).cinfo));
+    
+    
+    %% Calculate power and efficiency
+    
+    for ii = 1:length(wc)
+        
+        % evaluate mech performance
+        [Pmech_tot(ii), mPmech(:,ii)] = Pmech(wc(ii).ZL, Zpto, Zi, Fe);
+%         assert(-1*Pmech_tot(ii) <= sum(Pmax),...
+%             sprintf('''%s'' making more mechanical power than theoretical limit',wc(1).leg))
+        
+        % evaluate mech performance
+        [Pelec_tot(ii), mPelec(:,ii)] = Pelec(wc(ii).ZL, Zpto, Zi, Fe);
+%         assert(-1*Pelec_tot(ii) <= sum(Pmax),...
+%             sprintf('''%s'' making more mechanical power than theoretical limit',wc(1).leg))
+        
+        legCel{ii} = wc(ii).leg;
+    end
+    
+    eta_mech = Pmech_tot./(-1 * sum(Pmax));
+    eta_elec = Pelec_tot./(-1 * sum(Pmax));
+    
+    %% Tabulate results
+    
+    T = table(-1*Pmech_tot(:)/1e3,eta_mech(:),-1*Pelec_tot(:)/1e3,eta_elec(:),...
+        'VariableNames',{'MechPow_kW','MechEfficiency','ElecPow_kW','ElecEffciency'},...
+        'RowNames',legCel);
+    disp(T)
+    
 end
-legend(legCel)
+
+% clear input
+% input.data = T{:,1:end};
+% input.dataFormat = {'%.2g',};
+% input.tableColumnAlignment = 'c';
+% input.tableBorders = 0;
+% input.makeCompleteLatexDocument = 0;
+% input.tableRowLabels = T.Properties.RowNames;
+% input.tableCaption = 'Text caption';
+% input.tableLabel = 'tab:TestLabel';
+% input.tableColLabels = T.Properties.VariableNames;
+% latex = latexTable(input);
+%
+% %% Plot results
+%
+% figure('name','Frequency dependent power')
+% hold on
+% grid on
+% plot(mPmech,'-')
+% ax = gca;
+% ax.ColorOrderIndex = 1;
+% plot(mPelec,'--')
+%
+% figure('name','Bode plot')
+% hold on
+% grid on
+% opt = bodeoptions;
+% opt.FreqUnits = 'Hz';
+% opt.Grid = 'on';
+% for ii = 1:length(wc)
+%     bodeplot(frd(wc(ii).ZL,w),opt)
+%     legCel{ii} = wc(ii).leg;
+% end
+% legend(legCel)
