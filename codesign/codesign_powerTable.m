afef70d236342dca5c01eaa731ce76c994b6f0c9
Ryan Coe <rcoe@sandia.gov>
2020-09-19 13:50:21 -0600

    add license to script docstrings

--- codesign/codesign_powerTable.m
+++ codesign/codesign_powerTable.m
@@ -3,11 +3,25 @@
 % either the hydro-mechanical system (e.g., "CC on mech") or the electrical
 % system (e.g., "CC on elec") in a single sea state.
 
-% clc
-% clear
-% close all
-
-% plotflag = 1;
+% Copyright 2020 National Technology & Engineering Solutions of Sandia, LLC
+% (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+% Government retains certain rights in this software.
+%
+% This file is part of fbWecCntrl.
+%
+%     fbWecCntrl is free software: you can redistribute it and/or modify it
+%     under the terms of the GNU General Public License as published by the
+%     Free Software Foundation, either version 3 of the License, or (at
+%     your option) any later version.
+%
+%     fbWecCntrl is distributed in the hope that it will be useful, but
+%     WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%     General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with fbWecCntrl.  If not, see <https://www.gnu.org/licenses/>.
+% ---------------------------------------------------------------------
 
 %% Load WEC device data
 
